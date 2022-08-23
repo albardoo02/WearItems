f3e68c77d4285694433143a3188f07dca2a999bb
albardoo02 <albardoo02@gmail.com>
2022-08-23 21:10:01 +0900

    タブ補完追加

--- src/main/java/com/github/albardoo02/WearCommandExecutor.java
+++ src/main/java/com/github/albardoo02/WearCommandExecutor.java
@@ -4,11 +4,16 @@ import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.PlayerInventory;
 
-public class WearCommandExecutor implements CommandExecutor {
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class WearCommandExecutor implements CommandExecutor, TabCompleter {
 
     private Player player;
 
@@ -17,7 +22,7 @@ public class WearCommandExecutor implements CommandExecutor {
         if (sender instanceof Player) {
             player = (Player) sender;
         } else {
-            sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "コンソールからは実行できません!"));
+            sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&cコンソールからは実行できません!"));
             return true;
         }
         if(cmd.getName().equalsIgnoreCase("wear")){
@@ -58,5 +63,28 @@ public class WearCommandExecutor implements CommandExecutor {
             return true;
         }
         return true;
+    }
+    @Override
+    public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
+        if(command.getName().equalsIgnoreCase("wear")) {
+            if (args.length == 1) {
+                if (args[0].length() == 0) {
+                    return Arrays.asList("helmet", "chestplate", "leggings", "boots");
+                } else {
+                    if ("helmet".startsWith(args[0]) && "chestplate".startsWith(args[0]) && "leggings".startsWith(args[0]) && "leggings".startsWith(args[0]) && "boots".startsWith(args[0])) {
+                        return Arrays.asList("helmet", "chestplate", "leggings", "boots");
+                    } else if ("helmet".startsWith(args[0])) {
+                        return Collections.singletonList("helmet");
+                    } else if ("chestplate".startsWith(args[0])) {
+                        return Collections.singletonList("chestplate");
+                    } else if ("leggings".startsWith(args[0])) {
+                        return Collections.singletonList("leggings");
+                    } else if ("boots".startsWith(args[0])) {
+                        return Collections.singletonList("boots");
+                    }
+                }
+            }
+        }
+        return null;
     }
 }
